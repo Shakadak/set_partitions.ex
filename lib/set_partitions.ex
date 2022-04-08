2dafbc3e4e7e3764904c0800575a7f4ff51448cf
nathanael <nathanael.pineau@gmail.com>
2022-04-08 16:50:01 +0200

    mix format

--- lib/set_partitions.ex
+++ lib/set_partitions.ex
@@ -10,12 +10,14 @@ defmodule SetPartitions do
       state
     end
   end
+
   def for_n(:asc, from: n, to: m) do
     Stream.unfold(n, fn
       n when n <= m -> {n, n + 1}
       _ -> nil
     end)
   end
+
   def for_n(:desc, from: n, to: m) do
     Stream.unfold(n, fn
       n when n >= m -> {n, n - 1}
@@ -32,27 +34,34 @@ defmodule SetPartitions do
   def initialize_last(n) do
     k = Tuple.duplicate(0, n)
     m = k
-    Enum.reduce(0 .. n - 1, {k, m}, fn i, {k, m} ->
+
+    Enum.reduce(0..(n - 1), {k, m}, fn i, {k, m} ->
       {put_elem(k, i, i), put_elem(m, i, i)}
     end)
   end
 
   def next_partition({k, m}), do: next_partition(k, m)
+
   def next_partition(k, m) do
     n = tuple_size(k)
+
     Enum.reduce_while(for_n(:desc, from: n - 1, to: 1), :fail, fn i, :fail ->
       k0 = elem(k, 0)
       ki = elem(k, i)
       mi = elem(m, i)
+
       if ki <= elem(m, i - 1) do
         k = put_elem(k, i, ki + 1)
         m = put_elem(m, i, max(mi, elem(k, i)))
         mi = elem(m, i)
-        km = Enum.reduce(for_n(:asc, from: i + 1, to: n - 1), {k, m}, fn j, {k, m} ->
-          k = put_elem(k, j, k0)
-          m = put_elem(m, j, mi)
-          {k, m}
-        end)
+
+        km =
+          Enum.reduce(for_n(:asc, from: i + 1, to: n - 1), {k, m}, fn j, {k, m} ->
+            k = put_elem(k, j, k0)
+            m = put_elem(m, j, mi)
+            {k, m}
+          end)
+
         {:halt, km}
       else
         {:cont, :fail}
@@ -65,48 +74,65 @@ defmodule SetPartitions do
   def p_initialize_first(n, p) when p <= 0 or p > n do
     raise("p must be in 1..#{n}, received: #{p}")
   end
+
   def p_initialize_first(n, p) do
     k = Tuple.duplicate(0, n)
     m = k
-    {k, m} = Enum.reduce(for_n(:asc, from: n - p + 1, to: n - 1), {k, m}, fn i, {k, m} ->
-      {put_elem(k, i, i - (n - p)), put_elem(m, i, i - (n - p))}
-    end)
+
+    {k, m} =
+      Enum.reduce(for_n(:asc, from: n - p + 1, to: n - 1), {k, m}, fn i, {k, m} ->
+        {put_elem(k, i, i - (n - p)), put_elem(m, i, i - (n - p))}
+      end)
+
     {k, m, p}
   end
 
   def p_initialize_last(n, p) do
     k = Tuple.duplicate(0, n)
     m = k
-    {k, m} = Enum.reduce(for_n(:asc, from: 0, to: n - p), {k, m}, fn i, {k, m} ->
-      {put_elem(k, i, i), put_elem(m, i, i)}
-    end)
-    {k, m} = Enum.reduce(for_n(:asc, from: n - p + 1, to: n - 1), {k, m}, fn i, {k, m} ->
-      {put_elem(k, i, p - 1), put_elem(m, i, p - 1)}
-    end)
+
+    {k, m} =
+      Enum.reduce(for_n(:asc, from: 0, to: n - p), {k, m}, fn i, {k, m} ->
+        {put_elem(k, i, i), put_elem(m, i, i)}
+      end)
+
+    {k, m} =
+      Enum.reduce(for_n(:asc, from: n - p + 1, to: n - 1), {k, m}, fn i, {k, m} ->
+        {put_elem(k, i, p - 1), put_elem(m, i, p - 1)}
+      end)
+
     {k, m, p}
   end
 
   def p_next_partition({k, m, p}), do: p_next_partition(k, m, p)
+
   def p_next_partition(k, m, p) do
     n = tuple_size(k)
+
     Enum.reduce_while(for_n(:desc, from: n - 1, to: 1), :fail, fn i, :fail ->
       k0 = elem(k, 0)
       ki = elem(k, i)
       mi = elem(m, i)
+
       if ki < p - 1 and ki <= elem(m, i - 1) do
         k = put_elem(k, i, ki + 1)
         m = put_elem(m, i, max(mi, elem(k, i)))
         mi = elem(m, i)
-        {k, m} = Enum.reduce(for_n(:asc, from: i + 1, to: n - (p - mi)), {k, m}, fn j, {k, m} ->
-          k = put_elem(k, j, k0)
-          m = put_elem(m, j, mi)
-          {k, m}
-        end)
-        {k, m} = Enum.reduce(for_n(:asc, from: n - (p - mi) + 1, to: n - 1), {k, m}, fn j, {k, m} ->
-          k = put_elem(k, j, p - (n - j))
-          m = put_elem(m, j, p - (n - j))
-          {k, m}
-        end)
+
+        {k, m} =
+          Enum.reduce(for_n(:asc, from: i + 1, to: n - (p - mi)), {k, m}, fn j, {k, m} ->
+            k = put_elem(k, j, k0)
+            m = put_elem(m, j, mi)
+            {k, m}
+          end)
+
+        {k, m} =
+          Enum.reduce(for_n(:asc, from: n - (p - mi) + 1, to: n - 1), {k, m}, fn j, {k, m} ->
+            k = put_elem(k, j, p - (n - j))
+            m = put_elem(m, j, p - (n - j))
+            {k, m}
+          end)
+
         {:halt, {k, m, p}}
       else
         {:cont, :fail}
